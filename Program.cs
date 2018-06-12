be81523f2b26b804e8b7a55aa3786784bdc7a03b
test <test@runoob.com>
2018-06-13 00:51:11 +0800

    tijiao

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace ApiGateway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             IWebHostBuilder builder = new WebHostBuilder();
+
+            builder.ConfigureServices(s => {
+                s.AddSingleton(builder);
+            });
+
+            builder.UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>();          
+           var host = builder.Build();
+            
+            host.Run();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using CacheManager.Core;
+using ConfigurationBuilder = Microsoft.Extensions.Configuration.ConfigurationBuilder;
+using System;
+
+namespace ApiGateway{  
+
+  public class Startup
+    {       
+   public Startup(IHostingEnvironment env)        {       
+        var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddJsonFile("configuration.json")
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }      
+        
+       public IConfigurationRoot Configuration { get; }     
+         
+       public void ConfigureServices(IServiceCollection services)        {
+            services.AddOcelot(Configuration); //此处添加Ocelot服务
+        }      
+       
+      public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+
+            app.UseOcelot().Wait();//此处使用Ocelot服务
+        }
+    }
+}
